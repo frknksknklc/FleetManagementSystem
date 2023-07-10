c0f5991aa7c61e806821e73121ca2abbb23e2589
Furkan Keskinkilic <furkankeskinkilic97@gmail.com>
2023-07-10 23:11:03 +0300

    Create Routes to the All the Modules

--- fleetms/src/main/java/com/kindsonthegenius/fleetms/ApplicationController.java
+++ fleetms/src/main/java/com/kindsonthegenius/fleetms/ApplicationController.java
@@ -2,8 +2,10 @@ package com.kindsonthegenius.fleetms;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping
 public class ApplicationController {
 	
 	@GetMapping("/index")
@@ -11,15 +13,34 @@ public class ApplicationController {
 		return "index";
 	}
 
+	@GetMapping("/fleet")
+	public String fleet() {
+		return "/fleet/index";
+	}
+	
+	@GetMapping("/helpdesk")
+	public String helpdesk() {
+		return "/helpdesk/index";
+	}
+	
+	@GetMapping("/accounts")
+	public String accounts() {
+		return "/accounts/index";
+	}
+	
+	@GetMapping("/payroll")
+	public String payroll() {
+		return "/payroll/index";
+	}
+	
 	@GetMapping("/hr")
 	public String widget() {
 		return "/hr/index";
 	}
 	
-//	@GetMapping("/hr")
-//	public String hr() {
-//		return "/hr/index";
-//	}
-	
+	@GetMapping("/parameters")
+	public String parameters() {
+		return "/parameters/index";
+	}
 
 }
